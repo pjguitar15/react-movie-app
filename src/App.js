a92c89dbc980b700ed99f96ea55ba7aedb28a059
Phil <pjguitar15@gmail.com>
2021-03-05 12:37:47 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import MovieCard from './components/MovieCard.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-center text-light'>Movies</h1>
+      <MovieCard />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import { Col, Row, Container } from 'react-bootstrap'
+const MovieCard = () => {
+  const [featured, setFeatured] = useState([])
+  const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&`
+  const IMAGES_API = `https://image.tmdb.org/t/p/w1280`
+  const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&`
+  const API_KEY = `api_key=3997d18b9ba6f2c5d2b91c4d53d096f0`
+  useEffect(() => {
+    fetch(FEATURED_API + API_KEY)
+      .then((res) => res.json())
+      .then((data) => setFeatured(data.results))
+
+    console.log(featured.title)
+  }, [])
+  return (
+    <Container>
+      <Row>
+        {featured.map((item, index) => (
+          <Col key={index} xl='3' className='my-4'>
+            <img
+              style={{ width: '100%' }}
+              src={IMAGES_API + item.poster_path}
+              alt='movieposter'
+            />
+            <div
+              style={{ background: '#263859' }}
+              className='d-flex justify-content-between align-items-center text-light py-3 px-3'
+            >
+              <h6>{item.title}</h6>{' '}
+              <span
+                style={{
+                  background: '#17223B',
+                  padding: '3px 20px',
+                  borderRadius: '7px',
+                }}
+              >
+                8.6
+              </span>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  )
+}
+
+export default MovieCard
