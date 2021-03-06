9666f8ceb934708534988ba35600791895ee09a5
Phil <pjguitar15@gmail.com>
2021-03-06 16:00:41 +0800

    added subtitle

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const NavbarComponent = ({ submitHandler, searchTerm, setSearchTerm }) => {
   return (
     <div className='NavbarComponent'>
-      <h1>My Movies</h1>
+      <div>
+        <h1>My Movies</h1>
+        <div className='text-secondary font-italic'>
+          created by Philcob | API: The Movie Db
+        </div>
+      </div>
       <form onSubmit={submitHandler}>
         <input
           value={searchTerm}
